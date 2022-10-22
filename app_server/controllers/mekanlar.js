ff2c29a1a9d2513160bfd12eaedea9f65147246f
seydaydiin <seydaydiin0@gmail.com>
2022-10-22 23:03:01 +0300

    odev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,11 +1,75 @@
 var express = require('express');
 var router = express.Router();
 const anaSayfa = function(req, res, next) {
-    res.render('anasayfa', { title: 'Anasayfa' });
+    res.render('anasayfa', 
+    {
+    "baslik" : 'Anasayfa', 
+    "sayfaBaslik" : {
+    "siteAd" :"MekanBul" ,
+    "slogan" : "Civardaki Mekanları Keşfet!"
+    },
+    "mekanlar" : [
+        {
+            "ad" : "Starbucks",
+            "puan": "4",
+            "adres" :"Centrum Garden AVM",
+            "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
+            "mesafe" : "10km"
+        },
+        {
+            "ad" : "Gloria Jeans",
+            "puan": "3",
+            "adres" :"Sdü Doğu Kampüsü",
+            "imkanlar":["Kahve","Çay","Pasta"],
+            "mesafe" : "5km"
+        }
+
+    ]
+});
 };
 
 const mekanBilgisi = function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan bilgisi' });
+    res.render('mekanbilgisi',
+    { "baslik": 'Mekan bilgisi',
+    "mekanBaslik":"Starbucks",
+    "mekanDetay": {
+        "ad":"Starbucks",
+        "adres":"Centrum Garden AVM",
+        "puan":"4",
+        "imkanlar" : ["Dünya Kahveleleri","Kek","Pasta"],
+        "koordinatlar":{
+            "enlem":"37.7",
+            "boylam": "30.5"
+        },
+        "saatler":[
+            {
+            "gunler":"Pazartesi-Cuma",
+            "acilis":"09:00",
+            "kapanis":"23:00",
+            "kapali":false
+        },
+        {
+            "gunler":"Cumartesi-Pazar",
+            "acilis":"10:00",
+            "kapanis":"22:00",
+            "kapali":false
+        }
+        ],
+        "yorumlar":[
+            {
+                "yorumYapan":"Şeyda AYDIN",
+                "yorumMetni":"Kahveler iyiiii.",
+                "tarih":"22 Ekim 2022",
+                "puan":"5"
+            }
+        ]
+
+        
+    
+        
+    }
+
+});
 };
 
 const yorumEkle = function(req, res, next) {
